a9f4de6203cf2fc02ab5222c6e09d815bdf2c3bb
ouzhenxuan <442910407@qq.com>
2016-07-26 22:34:57 +0800

    Initial Commit

---
+++ 3DPictureRotation/ViewController.h
@@ -0,0 +1,15 @@
+//
+//  ViewController.h
+//  3DPictureRotation
+//
+//  Created by 区振轩 on 16/7/26.
+//  Copyright © 2016年 区振轩. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface ViewController : UIViewController
+
+
+@end
+
---
+++ 3DPictureRotationTests/_DPictureRotationTests.m
@@ -0,0 +1,39 @@
+//
+//  _DPictureRotationTests.m
+//  3DPictureRotationTests
+//
+//  Created by 区振轩 on 16/7/26.
+//  Copyright © 2016年 区振轩. All rights reserved.
+//
+
+#import <XCTest/XCTest.h>
+
+@interface _DPictureRotationTests : XCTestCase
+
+@end
+
+@implementation _DPictureRotationTests
+
+- (void)setUp {
+    [super setUp];
+    // Put setup code here. This method is called before the invocation of each test method in the class.
+}
+
+- (void)tearDown {
+    // Put teardown code here. This method is called after the invocation of each test method in the class.
+    [super tearDown];
+}
+
+- (void)testExample {
+    // This is an example of a functional test case.
+    // Use XCTAssert and related functions to verify your tests produce the correct results.
+}
+
+- (void)testPerformanceExample {
+    // This is an example of a performance test case.
+    [self measureBlock:^{
+        // Put the code you want to measure the time of here.
+    }];
+}
+
+@end
